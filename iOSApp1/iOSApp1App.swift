49437dd717fba13225d599fb429bd308bab0900d
BNaugler317 <Brandon.Naugler@easternstudent.ca>
2026-05-21 09:25:07 -0300

    Initial Commit

---
+++ iOSApp1/iOSApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp1App.swift
+//  iOSApp1
+//
+//  Created by Brandon Naugler on 2026-05-21.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
