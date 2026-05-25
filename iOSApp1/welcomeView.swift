dffdb46fe1174a2d0f6f30543673a82c2500028f
BNaugler317 <Brandon.Naugler@easternstudent.ca>
2026-05-24 21:06:07 -0300

    Finished Assignment 1

---
+++ iOSApp1/welcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  welcomeView.swift
+//  iOSApp1
+//
+//  Created by Brandon Naugler on 2026-05-21.
+//
+
+import SwiftUI
+
+struct welcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    welcomeView()
+}
